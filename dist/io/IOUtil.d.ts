c39a08e1e69f15469010e6de82fc133cd0b254c7
Simon Townsend <simon.townsend1@ge.com>
2018-07-30 13:22:43 -0500

    dis

--- dist/io/IOUtil.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-export declare class IOUtil {
-    constructor();
-    static parseDataBuffer(dataBuffer: string | Buffer): string[];
-    static trimChars(s: string, c: string): string;
-}
